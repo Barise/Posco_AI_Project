d896ef9a6ad5ba08da3b86596dbf18e3f229063d
Chiron <qkfltp01@naver.com>
2020-08-11 15:19:14 +0900

    [+] connect mysql

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
+import pymysql
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
 @app.route("/")
+def my_index():
+    print("route")
+    return flask.render_template("index.html", token="Hello Flask+React")
+
+
+@app.route("/teo")
 def judgeBloodTest():
     print("TEO")
     return jsonify({'test': "hello world"})
@@ -16,20 +23,21 @@ def helloWorld():
     return "hello world"
 
 
-@app.route("/board/list")
+@app.route("/binlist")
 def board_list():
-    conn = pymysql.connect(host="localhost", user="ssafy",
-                           password="ssafy", db="itnews", charset="utf8")
+    conn = pymysql.connect(host="localhost", user="root",
+                           password="root", db="popang", charset="utf8")
 
     curs = conn.cursor(pymysql.cursors.DictCursor)
+    # sql = "select b.*, @rownum := @rownum+1 as rnum from board as b, (select @rownum := 0) as r"
 
-    sql = "select b.*, @rownum := @rownum+1 as rnum from board as b, (select @rownum := 0) as r"
+    sql = "select * from bin"
     curs.execute(sql)
 
     rows = curs.fetchall()
-
+    print(rows)
     return jsonify(rows)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="8000")
